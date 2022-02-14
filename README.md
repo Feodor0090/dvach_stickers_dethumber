3d0ae99ccd27ee066675bbe5066813f6ed1f3d0b
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-02-14 15:28:34 +0300

    Добавьте файлы проекта.

---
+++ README.md
@@ -0,0 +1,12 @@
+# 2ch stickers de-thumber
+
+Redownloads full sticker images instead of previews.
+
+## Usage
+
+1. Build using Visual Studio or your IDE of choise.
+2. Find a stickerpack, download it's page with content (in chrome - ctrl+s > full web page).
+3. Run this script.
+4. Paste path to `_files` folder of downloaded page. Must be without quotes!
+5. Paste URL of stickerset (see URL of any preview image, it's its directory).
+6. Your images are in `full` subfolder.
---
+++ dvach_stickers_dethumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace dvach_stickers_dethumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Папка _files: ");
+            Directory.SetCurrentDirectory(Console.ReadLine());
+            Console.Write("URL стикерпака (директория с пнг): ");
+            string baseUrl = Console.ReadLine();
+            Console.WriteLine("Обработка...");
+            var fs = new DirectoryInfo(Directory.GetCurrentDirectory()).EnumerateFiles("*_thumb.png");
+            Directory.CreateDirectory("full");
+            foreach (var f in fs)
+            {
+                string name = f.Name.Substring(0, f.Name.IndexOf("_thumb.png")) + ".png";
+                HttpWebRequest httpRequest = WebRequest.CreateHttp(baseUrl + name);
+                HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+                Stream httpResponseStream = httpResponse.GetResponseStream();
+
+                int bufferSize = 1024;
+                byte[] buffer = new byte[bufferSize];
+                int bytesRead = 0;
+
+                FileStream fileStream = File.Create("full/" + name);
+                while ((bytesRead = httpResponseStream.Read(buffer, 0, bufferSize)) != 0)
+                {
+                    fileStream.Write(buffer, 0, bytesRead);
+                }
+            }
+            Console.WriteLine("Готово.");
+            Thread.Sleep(200);
+        }
+    }
+}
